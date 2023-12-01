8b8aa695ec02e885b6f55bfb7bcc08cceedb7475
pratik-senjaliya <pratiksenjaliya1999@gmail.com>
2023-12-01 21:47:04 +0530

    image tool code completed

--- src/ImageTool.js
+++ src/ImageTool.js
@@ -10,7 +10,6 @@ function generateDownload(canvas, crop) {
     return;
   }
 
-
   canvas.toBlob(
     (blob) => {
       const previewUrl = window.URL.createObjectURL(blob);
@@ -32,30 +31,50 @@ export default function ImageTool(props) {
   const location = useLocation();
   const aspectParam = new URLSearchParams(location.search).get("aspect");
 
-  console.log("AspectParam", aspectParam);
   const selectOptions = [
     {
-      label: "16 / 9",
-      value: { unit: "%", width: 0, aspect: 16 / 9 },
+      label: "Free",
+      value: { unit: "%", width: 0 },
     },
     {
-      label: "4 /3",
-      value: { unit: "%", width: 0, aspect: 4 / 3 },
+      label: "1:1",
+      value: { unit: "%", width: 0, aspect: 1 / 1 },
+    },
+    // {
+    //   label: "3:2",
+    //   value: { unit: "%", width: 0, aspect: 3 / 2 },
+    // },
+    // {
+    //   label: "4:5",
+    //   value: { unit: "%", width: 0, aspect: 4 / 5 },
+    // },
+    // {
+    //   label: "4:6",
+    //   value: { unit: "%", width: 0, aspect: 4 / 6 },
+    // },
+    {
+      label: "5:3",
+      value: { unit: "%", width: 0, aspect: 5 / 3 },
+    },
+    {
+      label: "9:16",
+      value: { unit: "%", width: 0, aspect: 9 / 16 },
     },
   ];
   const [upImg, setUpImg] = useState();
-  const [showSelect, setShowSelect] = useState(false); 
-  const imgRef = useRef(null); 
-  const previewCanvasRef = useRef(null); 
-  const [crop, setCrop] = useState({ unit: "%", width: 0, aspect: 9 / 16 }); 
-  const [completedCrop, setCompletedCrop] = useState(null); 
- 
+  const [defaultOption, setDefaultOption] = useState();
+  const [showSelect, setShowSelect] = useState(false);
+  const imgRef = useRef(null);
+  const previewCanvasRef = useRef(null);
+  const [crop, setCrop] = useState({ unit: "%", width: 0, aspect: 9 / 16 });
+  const [completedCrop, setCompletedCrop] = useState(null);
+
   const onSelectFile = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       const reader = new FileReader();
       reader.addEventListener("load", () => setUpImg(reader.result));
       reader.readAsDataURL(e.target.files[0]);
-      setShowSelect(true); 
+      setShowSelect(true);
     }
   };
 
@@ -63,26 +82,25 @@ export default function ImageTool(props) {
     imgRef.current = img;
   }, []);
 
-
   useEffect(() => {
     if (!completedCrop || !previewCanvasRef.current || !imgRef.current) {
       return;
     }
 
-    const image = imgRef.current; 
-    const canvas = previewCanvasRef.current; 
-    const crop = completedCrop; 
+    const image = imgRef.current;
+    const canvas = previewCanvasRef.current;
+    const crop = completedCrop;
 
-    const scaleX = image.naturalWidth / image.width; 
+    const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
-    const ctx = canvas.getContext("2d"); 
-    const pixelRatio = window.devicePixelRatio; 
+    const ctx = canvas.getContext("2d");
+    const pixelRatio = window.devicePixelRatio;
 
     canvas.width = crop.width * pixelRatio;
     canvas.height = crop.height * pixelRatio;
 
     ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-    ctx.imageSmoothingQuality = "high"; 
+    ctx.imageSmoothingQuality = "high";
 
     ctx.drawImage(
       image,
@@ -97,13 +115,57 @@ export default function ImageTool(props) {
     );
   }, [completedCrop]);
 
+  useEffect(() => {
+    if (aspectParam === "5:3") {
+      setCrop({ unit: "%", width: 0, aspect: 5 / 3 });
+      setDefaultOption({
+        label: "5:3",
+        value: { unit: "%", width: 0, aspect: 5 / 3 },
+      });
+    }
+    if (aspectParam === "1:1") {
+      setCrop({ unit: "%", width: 0, aspect: 1 / 1 });
+      setDefaultOption({
+        label: "1:1",
+        value: { unit: "%", width: 0, aspect: 1 / 1 },
+      });
+    }
+    // if (aspectParam === "3:2") {
+    //   setCrop({ unit: "%", width: 0, aspect: 3 / 2 });
+    //   setDefaultOption({
+    //     label: "3:2",
+    //     value: { unit: "%", width: 0, aspect: 3 / 2 },
+    //   });
+    // }
+    // if (aspectParam === "4:5") {
+    //   setCrop({ unit: "%", width: 0, aspect: 4 / 5 });
+    //   setDefaultOption({
+    //     label: "4:5",
+    //     value: { unit: "%", width: 0, aspect: 4 / 5 },
+    //   });
+    // }
+    // if (aspectParam === "4:6") {
+    //   setCrop({ unit: "%", width: 0, aspect: 4 / 6 });
+    //   setDefaultOption({
+    //     label: "4:6",
+    //     value: { unit: "%", width: 0, aspect: 4 / 6 },
+    //   });
+    // }
+    if (aspectParam === "9:16") {
+      setCrop({ unit: "%", width: 0, aspect: 9 / 16 });
+      setDefaultOption({
+        label: "9:16",
+        value: { unit: "%", width: 0, aspect: 9 / 16 },
+      });
+    }
+  }, [aspectParam]);
 
   return (
     <div className="App">
       <div className="functions">
         <div>
           <label for="file-upload" class="custom-file-upload">
-            Upload
+            <span>Upload</span>
           </label>
           <input
             id="file-upload"
@@ -114,16 +176,15 @@ export default function ImageTool(props) {
         </div>
         {showSelect && (
           <Select
+          className="selectField"
             options={selectOptions}
+            defaultValue={defaultOption}
             onChange={(option) => setCrop(option?.value)}
-            defaultValue={{
-              value: { unit: "%", width: 30, aspect: 16 / 9 },
-              label: "16 / 9",
-            }}
           />
         )}
         <button
           type="button"
+          className="downloadButton"
           hidden={!completedCrop?.width || !completedCrop?.height}
           disabled={!completedCrop?.width || !completedCrop?.height}
           onClick={() =>
@@ -136,10 +197,10 @@ export default function ImageTool(props) {
 
       <ReactCrop
         src={upImg}
-        onImageLoaded={onLoad} 
+        onImageLoaded={onLoad}
         crop={crop}
-        onChange={(c) => setCrop(c)} 
-        onComplete={(c) => setCompletedCrop(c)} 
+        onChange={(c) => setCrop(c)}
+        onComplete={(c) => setCompletedCrop(c)}
       />
       <div className="canvas">
         <canvas
